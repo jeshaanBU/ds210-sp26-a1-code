f05a008ce7c266d6bb50d300ae79b08eb42fc3da
Mira Saldanha <mira6ts@bu.edu>
2026-02-23 15:58:04 -0500

    Student 2: Implement SlowVec remove

--- project_1_vector/slow_vec/src/lib.rs
+++ project_1_vector/slow_vec/src/lib.rs
@@ -64,8 +64,28 @@ impl<T> SlowVec<T> {
     }
 
     // Student 2: Provide your solution here
-    pub fn remove(&mut self, i: usize) {
-        todo!("Student 2 should implement this");
+    pub fn remove(&mut self, i: usize) -> T {
+        if i >= self.len() {
+            panic!("Index out of bounds");
+        }
+
+        let old_len = self.len();
+        let mut new_fixed = FixedSizeArray::allocate(old_len - 1);
+        
+        let mut removed: Option<T> = None;
+        let mut j = 0;
+
+        for k in 0..old_len {
+            if k == i {
+                removed = Some(self.fixed.move_out(k));
+            } else {
+                let val = self.fixed.move_out(k);
+                new_fixed.put(val, j);
+                j += 1;
+            }
+        }
+        self.fixed = new_fixed;
+        removed.unwrap()
     }
 }
 
