c817efe944cde7ce98ec49d6ec6c023406f13968
mira6ts-star <mira6ts@bu.edu>
2026-03-25 13:08:14 -0400

    Adding comments to v4
    Updated ChatbotV4 to handle user sessions with file storage.

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -1,10 +1,13 @@
 use kalosm::language::*;
 use crate::solution::file_library;
 
+// different than v3 because it does not store a HashMap of active sessions
 pub struct ChatbotV4 {
     model: Llama,
 }
 
+// stores the model
+// sessions are handled later during chat_with_user
 impl ChatbotV4 {
     pub fn new(model: Llama) -> ChatbotV4 {
         return ChatbotV4 {
@@ -13,8 +16,12 @@ impl ChatbotV4 {
     }
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
+        // make a filename from the username
+        // each user gets their own file
         let filename = &format!("{}.txt", username);
 
+        // creates a fresh chat session
+        // checks whether there is already a saved session on disk for that user.
         let mut chat_session: Chat<Llama> = self.model
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
@@ -23,6 +30,8 @@ impl ChatbotV4 {
         // If Some(session) comes back, restore it so the bot remembers past messages.
         // If None comes back (first time chatting), just keep the fresh session we made above.
 
+        // load_chat_session_from_file(&filename) tries to read the user’s old session from the file
+        // if SOME the code will rebuild the chat session using the saved session
         if let Some(session) = file_library::load_chat_session_from_file(&filename) {
             chat_session = self.model
                 .chat()
@@ -30,12 +39,19 @@ impl ChatbotV4 {
                 .with_session(session);
         }
 
+        // if none, there is no previous file so the fresh session stays
+
+        // adds user's message
+        // waits for model response
         let output = chat_session
             .add_message(message)
             .await;
 
         match output {
             Ok(response) => {
+                // gets the updates session
+                // save it to user's file
+                // returns the response
                 let session = chat_session
                     .session()
                     .expect("session should exist");
