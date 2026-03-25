e1cf020837238baf231004c45a34b8fb3fa4d33e
mira6ts-star <mira6ts@bu.edu>
2026-03-25 11:19:17 -0400

    Adding comments to v2 for code review

--- project_2_chatbot/basic_chatbot/src/solution/v2.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v2.rs
@@ -5,11 +5,16 @@ pub struct ChatbotV2 {
     // What should you store inside your Chatbot type?
     // The model? The chat_session?
     model: Llama,
+    // adds memory by storing the session in the structure. each new message is added to the same existing conversation
     chat_session: Chat<Llama>,
 }
 
 impl ChatbotV2 {
     #[allow(dead_code)]
+    // creates chat session once chatbot is first created
+    // DIFF BETWEEN V1 and V2: 
+    // V2: we create the session in new so it can be reused later
+    // V1: session created inside chat_with_user to it was recreated everytime
     pub fn new(model: Llama) -> ChatbotV2 {
         let chat_session = model
             .chat()
@@ -24,12 +29,16 @@ impl ChatbotV2 {
     }
 
     #[allow(dead_code)]
+    // adds new message directly to self.chat_session, which already contains earlier conversation history, so response depends on past messages
     pub async fn chat_with_user(&mut self, message: String) -> String {
-        // Add your code for chatting with the agent while keeping conversation history here.
+        // new message is being added to the existing saved session
         let output = self.chat_session.add_message(message).await;
 
         match output {
+            // If the model successfully returns a response, return it
             Ok(response) => response,
+
+            // If something goes wrong, return a fallback error message
             Err(_) => String::from("Sorry, I could not generate a response."),
         }
     }
