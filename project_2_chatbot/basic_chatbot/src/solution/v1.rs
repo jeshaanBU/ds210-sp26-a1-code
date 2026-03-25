4206a47e27d42f3076fe0ed07c1141d209c721e3
mira6ts-star <mira6ts@bu.edu>
2026-03-25 11:03:10 -0400

    Added comments to v1 for code review

--- project_2_chatbot/basic_chatbot/src/solution/v1.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v1.rs
@@ -1,5 +1,7 @@
 use kalosm::language::*;
 
+// only stores LLM model
+// does not store a chat session, history, usernames, files, or cache so we know that v1 doesn't store memory
 #[allow(dead_code)]
 pub struct ChatbotV1 {
     model: Llama,
@@ -7,25 +9,35 @@ pub struct ChatbotV1 {
 
 impl ChatbotV1 {
     #[allow(dead_code)]
+    // Initializes the chatbot with a Llama model so we can use that model later to start chats.
     pub fn new(model: Llama) -> ChatbotV1 {
         return ChatbotV1 { model: model };
     }
 
+    
     #[allow(dead_code)]
+    // takes in &mut self so it can use the chatbot object
+    // takes in message: String which is the user's message
+    // returns a string
+    // async because generating a response from the model takes time, so Rust needs to wait for the result with .await
     pub async fn chat_with_user(&mut self, message: String) -> String {
+        
+        // 1. takes stored model 2. starts a new chat session 3. gives it a system prompt so the assistant acts like a pirate
+        //every time the function is called, a new chat_session is created from scratch - so after the function ends, the chat session will disappear
+        // local variable 
         let mut chat_session: Chat<Llama> = self.model
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
-        
-        // You need to add your code here
-        // You must find a way to add the given message to the chat_session!
-        // consider https://docs.rs/kalosm/0.4.0/kalosm/language/struct.Chat.html#method.add_message
-        // Hint: make sure you transform/extract the response message as a **String**.
 
+        // adds the user’s input to the session and asks the model to generate a reply in that same session context
         let output = chat_session.add_message(message).await;
 
+        // handles output cases
         match output {
+            // If the model successfully returns a response, return it
             Ok(response) => response,
+
+            // If something goes wrong, return a fallback error message
             Err(_) => String::from("Sorry, I could not generate a response."),
         }
     }
