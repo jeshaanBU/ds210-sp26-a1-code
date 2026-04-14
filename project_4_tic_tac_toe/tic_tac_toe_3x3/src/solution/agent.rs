c37e463571ae9224f1bc23f1778c7961e1ba8547
Eshaan <jeshaan05@gmail.com>
2026-04-13 20:25:27 -0400

    Add minimax base case and scaffolding for 3x3 solution

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -2,17 +2,25 @@ use tic_tac_toe_stencil::agents::Agent;
 use tic_tac_toe_stencil::board::Board;
 use tic_tac_toe_stencil::player::Player;
 
-// Your solution solution.
 pub struct SolutionAgent {}
 
-// Put your solution here.
 impl Agent for SolutionAgent {
-    // Should returns (<score>, <x>, <y>)
-    // where <score> is your estimate for the score of the game
-    // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player) -> (i32, usize, usize) {
-        // If you want to make a recursive call to this solution, use
-        // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+        if board.game_over() {
+            return (board.score(), 0, 0);
+        }
+
+        let moves = board.moves();
+        let mut best_move = moves[0];
+        let mut best_score = match player {
+            Player::X => i32::MIN,
+            Player::O => i32::MAX,
+        };
+
+        for m in moves {
+            todo!()
+        }
+
+        (best_score, best_move.0, best_move.1)
     }
 }
